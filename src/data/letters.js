ddc76a332b48efa33c81b7f6e6b4f6f23ca246fe
cheeriomaster <ethan.4120@gmail.com>
2026-04-14 15:29:42 +0800

    Initial commit - tibetan-learn v1.0

---
+++ src/data/letters.js
@@ -0,0 +1,39 @@
+/**
+ * 藏文30个辅音字母数据
+ * 命名规则: {序号}_{Wylie转写}.mp3
+ * 如 1_ka.mp3, 2_kha.mp3, ...
+ */
+export const letters = [
+  { id: 1,  char: "ཀ", wylie: "ka",  name: "卡",  audio: "1_ka.mp3" },
+  { id: 2,  char: "ཁ", wylie: "kha", name: "喀",  audio: "2_kha.mp3" },
+  { id: 3,  char: "ག", wylie: "ga",  name: "嘎",  audio: "3_ga.mp3" },
+  { id: 4,  char: "ང", wylie: "nga", name: "昂",  audio: "4_nga.mp3" },
+  { id: 5,  char: "ཅ", wylie: "ca",  name: "匝",  audio: "5_ca.mp3" },
+  { id: 6,  char: "ཆ", wylie: "cha", name: "叉",  audio: "6_cha.mp3" },
+  { id: 7,  char: "ཇ", wylie: "ja",  name: "扎",  audio: "7_ja.mp3" },
+  { id: 8,  char: "ཉ", wylie: "nya", name: "娘",  audio: "8_nya.mp3" },
+  { id: 9,  char: "ཏ", wylie: "ta",  name: "塔",  audio: "9_ta.mp3" },
+  { id: 10, char: "ཐ", wylie: "tha", name: "他",  audio: "10_tha.mp3" },
+  { id: 11, char: "ད", wylie: "da",  name: "达",  audio: "11_da.mp3" },
+  { id: 12, char: "ན", wylie: "na",  name: "那",  audio: "12_na.mp3" },
+  { id: 13, char: "པ", wylie: "pa",  name: "八",  audio: "13_pa.mp3" },
+  { id: 14, char: "ཕ", wylie: "pha", name: "趴",  audio: "14_pha.mp3" },
+  { id: 15, char: "བ", wylie: "ba",  name: "巴",  audio: "15_ba.mp3" },
+  { id: 16, char: "མ", wylie: "ma",  name: "妈",  audio: "16_ma.mp3" },
+  { id: 17, char: "ཙ", wylie: "tsa", name: "匝",  audio: "17_tsa.mp3" },
+  { id: 18, char: "ཚ", wylie: "tsha",name: "擦",  audio: "18_tsha.mp3" },
+  { id: 19, char: "ཛ", wylie: "dza", name: "杂",  audio: "19_dza.mp3" },
+  { id: 20, char: "ཝ", wylie: "wa",  name: "蛙",  audio: "20_wa.mp3" },
+  { id: 21, char: "ཞ", wylie: "zha", name: "沙",  audio: "21_zha.mp3" },
+  { id: 22, char: "ཟ", wylie: "za",  name: "杂",  audio: "22_za.mp3" },
+  { id: 23, char: "འ", wylie: "'a", name: "阿",  audio: "23_a.mp3" },
+  { id: 24, char: "ཡ", wylie: "ya",  name: "呀",  audio: "24_ya.mp3" },
+  { id: 25, char: "ར", wylie: "ra",  name: "热",  audio: "25_ra.mp3" },
+  { id: 26, char: "ལ", wylie: "la",  name: "拉",  audio: "26_la.mp3" },
+  { id: 27, char: "ཤ", wylie: "sha", name: "沙",  audio: "27_sha.mp3" },
+  { id: 28, char: "ས", wylie: "sa",  name: "撒",  audio: "28_sa.mp3" },
+  { id: 29, char: "ཧ", wylie: "ha",  name: "哈",  audio: "29_ha.mp3" },
+  { id: 30, char: "ཨ", wylie: "a",  name: "阿",  audio: "30_a.mp3" }
+];
+
+export default letters;
